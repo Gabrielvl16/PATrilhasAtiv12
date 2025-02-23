02fd097b7b1986ea0e69e3d9eecf10bd3317b4d8
Gabriel Valú <gabrielvaluu@gmail.com>
2025-02-23 14:21:06 -0300

    Segundo commit
    Crie uma lista de strings e use o foreach para imprimir cada elemento na tela.

---
+++ B.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lista = { "A", "B", "C", "D", "E" };
+
+        foreach (String cont in lista)
+        {
+            Console.WriteLine(cont);
+        }
+    }
+}
