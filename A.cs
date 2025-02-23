2978376f4870805098fdf29c58e2f18e16a7959d
Gabriel Valú <gabrielvaluu@gmail.com>
2025-02-23 14:15:45 -0300

    Primeiro commit
    Crie um array de inteiros e use o foreach para imprimir cada elemento na tela.

---
+++ A.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayInts = { 55, 66, 77, 88, 99 };
+        
+        foreach (int i in arrayInts)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
